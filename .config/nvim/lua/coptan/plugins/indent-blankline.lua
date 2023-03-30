5f06eb383dd05fa19952630cbea65073c78e358f
Coptan99 <omarcoptan9@gmail.com>
2023-03-30 21:40:09 +0200

    import my dotfiles

---
+++ .config/nvim/lua/coptan/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+local setup, indent_blankline = pcall(require, "indent_blankline")
+if not setup then
+  return
+end
+
+indent_blankline.setup({
+  show_end_of_line = true,
+  space_char_blankline = " ",
+})
+
